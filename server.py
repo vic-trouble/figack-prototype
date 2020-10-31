f8a47b9cc10fa52e6914943d6b065babd61fa453
Vic <vic.trouble@gmail.com>
2020-10-31 14:23:33 +0300

    Added coarse-grained lock

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import threading
+
 from model import *
 from ops import *
 from protocol import *
@@ -9,29 +11,31 @@ PLAYER_CHAR_INIT_HP = 10
 class Server:
     def __init__(self):
         self._games = []
+        self._lock = threading.Lock()
 
     def serve(self, request):
-        if isinstance(request, CreateGameRequest):
-            game = self._create_game()
-            self._games.append(game)
-            game_token = len(self._games)
+        with self._lock:
+            if isinstance(request, CreateGameRequest):
+                game = self._create_game()
+                self._games.append(game)
+                game_token = len(self._games)
 
-            player = GameOp(game).add_player(request.player_name)
-            GameOp(game).spawn_unit(hp=PLAYER_CHAR_INIT_HP, player_id=player.id)
+                player = GameOp(game).add_player(request.player_name)
+                GameOp(game).spawn_unit(hp=PLAYER_CHAR_INIT_HP, player_id=player.id)
 
-            return CreateGameResponse(game_token, player.id)
-        
-        elif isinstance(request, GetGameRequest):
-            return GetGameResponse(self._get_game(request.game_token))
-        
-        elif isinstance(request, JoinGameRequest):
-            game = self._get_game(request.game_token)
-            player = GameOp(game).add_player(request.player_name)
-            GameOp(game).spawn_unit(hp=PLAYER_CHAR_INIT_HP, player_id=player.id)
-            return JoinGameResponse(player.id)
+                return CreateGameResponse(game_token, player.id)
+            
+            elif isinstance(request, GetGameRequest):
+                return GetGameResponse(self._get_game(request.game_token))
+            
+            elif isinstance(request, JoinGameRequest):
+                game = self._get_game(request.game_token)
+                player = GameOp(game).add_player(request.player_name)
+                GameOp(game).spawn_unit(hp=PLAYER_CHAR_INIT_HP, player_id=player.id)
+                return JoinGameResponse(player.id)
 
-        else:
-            raise RuntimeError('Unknown request %s', type(request))
+            else:
+                raise RuntimeError('Unknown request %s', type(request))
 
     def _create_game(self):
         game = Game()
